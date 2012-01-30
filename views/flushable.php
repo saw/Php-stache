01e8dc203d8df060cf02df2ac617600b25e4d007
Stephen Woods <saw@stephenwoods.net>
2012-01-29 22:57:43 -0800

    some missing stuff

---
+++ views/flushable.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scaleble=no"/>
+	<link rel="stylesheet" href="/css/deploy.source.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	
+	<title>awesum</title>
+
+</head>
